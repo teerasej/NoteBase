81406ceffda35ead27bcaced6420fc53be2243b4
Teerasej <teerasej@gmail.com>
2017-06-20 22:30:17 +0700

    Rename for more readable file's name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { firebaseEnvironment } from './environment';
+import { firebaseEnvironment } from './firebase-environment';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
